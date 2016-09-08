963b22c704b863303d3d829636c03374b1c4d25c
wwswqswh <wwswqswh@163.com>
2016-09-08 19:44:31 +0800

    Add files via upload

---
+++ JsonCompareTest.java
@@ -0,0 +1,136 @@
+package com.travelsky.phoenix.jsonCompareTest;
+
+import java.lang.reflect.Type;
+import java.util.Date;
+import java.util.List;
+
+
+import org.junit.Test;
+
+import com.alibaba.fastjson.JSON;
+import com.ceair.observersdk.utils.JsonMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.travelsky.phoenix.jacksonUtilTest.JMapper;
+
+
+public class JsonCompareTest {
+
+	private SampleBuilder samplebuilder = new SampleBuilder();
+	private static String[] jsonName ={"Jackson","fastJSON","GSON"};
+	@Test 
+	public void testInitial(){
+		samplebuilder.sampleBuilderInitial(10,10,10);
+	}
+	@Test
+	public void testJson2Object(){
+		//Jackson类型
+		ObjectMapper mapper = new ObjectMapper();
+		//Gson时间需要单独设置
+		GsonBuilder builder = new GsonBuilder();
+		// Register an adapter to manage the date types as long values
+		builder.registerTypeAdapter(Date.class, new JsonDeserializer<Date>() {
+			public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) {
+				return new Date(json.getAsJsonPrimitive().getAsLong());
+			}
+		});
+		Gson gson = builder.create();
+
+		long[][] timecost = new long[10][3];
+		//设置开始时间
+		long starttime = 0;
+		//从文件读取数据
+		List<String> jsonList = samplebuilder.loadJSONSamples("d:\\samples_json.dat");
+
+		//执行10次大循环
+		for(int i=0;i<10;i++){
+			//Jackson	
+			starttime=System.currentTimeMillis();
+			for(String str:jsonList){	
+				try {
+					mapper.readValue(str, SampleEntity.class); 
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			timecost[i][0]=System.currentTimeMillis()-starttime;
+
+			//fastJson
+//			starttime=System.currentTimeMillis();
+//			for(String str:jsonList){	
+//				JSON.parseObject(str, SampleEntity.class);
+//			}
+//			timecost[i][1]=System.currentTimeMillis()-starttime;
+
+			//GSON
+//			starttime=System.currentTimeMillis();
+//			for(String str:jsonList){	
+//				gson.fromJson(str, SampleEntity.class);
+//			}
+//			timecost[i][2]=System.currentTimeMillis()-starttime;
+		}
+		for(int j=0;j<3;j++){
+			System.out.println(jsonName[j]);
+			for(int i=0;i<10;i++){
+				System.out.println(timecost[i][j]+" ");
+			}
+			System.out.println("\n");
+		}
+
+	}
+
+	@Test
+	public void testObject2JSON(){
+
+		ObjectMapper mapper = new ObjectMapper(); 
+		Gson gson = new Gson();
+
+		long[][] timecost = new long[10][3];
+		//设置开始时间
+		long starttime = 0;
+
+		//生成需要转换的数据
+		List<SampleEntity> list = samplebuilder.buildSamples(10000, 10, 10);
+		//执行10次大循环
+		for(int i=0;i<10;i++){
+			//Jackson	
+			starttime=System.currentTimeMillis();
+			for(SampleEntity entity:list){	
+				try {
+					mapper.writeValueAsString(entity);
+				} catch (JsonProcessingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			timecost[i][0]=System.currentTimeMillis()-starttime;
+
+			//fastJson
+			starttime=System.currentTimeMillis();
+			for(SampleEntity entity:list){	
+				JSON.toJSONString(entity);
+			}
+			timecost[i][1]=System.currentTimeMillis()-starttime;
+
+			//GSON
+			starttime=System.currentTimeMillis();
+			for(SampleEntity entity:list){	
+				gson.toJson(entity);
+			}
+			timecost[i][2]=System.currentTimeMillis()-starttime;
+		}
+		for(int j=0;j<3;j++){
+			System.out.println(jsonName[j]);
+			for(int i=0;i<10;i++){
+				System.out.println(timecost[i][j]+" ");
+			}
+			System.out.println("\n");
+		}
+	}
+}
---
+++ SampleBuilder.java
@@ -0,0 +1,128 @@
+package com.travelsky.phoenix.jsonCompareTest;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+
+import com.alibaba.fastjson.JSON;
+
+
+/**
+ * 创建样本
+ * @author accountwcx@qq.com
+ *
+ */
+public class SampleBuilder {
+
+	/**
+	 * 创建样本，并把样本保存到文件中。
+	 * 
+	 * @param sampleSize 样本数量
+	 * @param listSize 样本List长度
+	 * @param mapKeyNum 样本Map中Key的数量
+	 */
+	public void sampleBuilderInitial(int sampleSize, int listSize, int mapKeyNum){
+		
+		String jsonDataPath = "d:\\samples_json.dat";
+		buildJsonSamples(sampleSize, listSize, mapKeyNum, jsonDataPath);
+	}
+
+public List<String> loadJSONSamples(String filePath) {
+	List<String> list = new LinkedList<String>();
+
+	File file = new File(filePath);
+	if (!file.exists()) {
+		return list;
+	}
+
+	BufferedReader br = null;
+	try {
+		br = new BufferedReader(new FileReader(file));
+		String line = br.readLine();            
+		while(line != null){
+			list.add(line);
+			line = br.readLine();
+		}           
+	} catch (Exception e) {
+		e.printStackTrace();
+	} finally {
+		if (null != br) {
+			try {
+				br.close();
+			} catch (IOException e) {
+			}
+		}
+	}
+	return list;
+}
+
+/**
+ * 创建样本
+ * 
+ * @param sampleSize 样本数量
+ * @param listSize 样本List长度
+ * @param mapKeyNum 样本Map的Key数量
+ * @return 样本List
+ */
+public List<SampleEntity> buildSamples(int sampleSize, int listSize, int mapKeyNum) {
+	List<SampleEntity> list = new LinkedList<SampleEntity>();
+	for (int i = 0; i < sampleSize; i++) {
+		list.add(new SampleEntity(listSize, mapKeyNum));
+	}
+	return list;
+}
+
+/**
+ * 创建样本，并把样本JSON序列化，保存到文件中。
+ * 
+ * @param sampleSize 样本数量
+ * @param listSize 样本List长度
+ * @param mapKeyNum 样本Map中Key的数量
+ * @param filePath 样本输出的文件路径 
+ */
+public void buildJsonSamples(int sampleSize, int listSize, int mapKeyNum, String filePath) {
+	File file = new File(filePath);
+	File parent = file.getParentFile();
+	if (!parent.exists()) {
+		parent.mkdirs();
+	}
+
+	if (file.exists()) {
+		file.delete();
+	}
+
+	List<SampleEntity> list = buildSamples(sampleSize, listSize, mapKeyNum);
+
+	StringBuilder sb = new StringBuilder();
+	for (SampleEntity item : list) {
+		sb.append(JSON.toJSONString(item));
+		sb.append("\n");
+	}
+
+	BufferedWriter bw = null;
+	try {
+		file.createNewFile();
+
+		bw = new BufferedWriter(new FileWriter(file));
+		bw.write(sb.toString());
+		bw.flush();
+	} catch (IOException e) {
+		e.printStackTrace();
+	} finally {
+		if (null != bw) {
+			try {
+				bw.close();
+			} catch (IOException e) {
+			}
+		}
+	}
+}
+
+
+}
